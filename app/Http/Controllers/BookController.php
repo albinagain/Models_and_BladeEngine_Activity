dce778adffaa545e1e301c20cd479a4c6b9761fd
albin <ajmmosawan@gmail.com>
2024-04-08 13:46:32 +0800

    made index function in BookController

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Book;
 
 class BookController extends Controller
 {
-    //
+    public function index(){
+        $data = Book::all();
+        return view('booklist', ['date' => $data]);
+    }
 }
