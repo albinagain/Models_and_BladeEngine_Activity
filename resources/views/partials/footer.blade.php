e8ab701f421833b49f4a0bd9cb5bf78f36fa7cce
albin <ajmmosawan@gmail.com>
2024-04-08 14:18:44 +0800

    styled header and footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,2 +1,6 @@
+<footer class="bg-blue-600 h-16 flex flex-col justify-center items-end">
+    <h1 class="text-3xl text-slate-200 pr-7">List of Books</h1>
+</footer>
+</div>
 </body>
 </html>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Booklist</title>
     @vite('resources/css/app.css')
 </head>
-<body>
-    
+<body class="bg-slate-200">
+    <div class="container mx-auto">
+        <header class="bg-blue-600 h-16 flex flex-col justify-center items-start">
+            <h1 class="text-3xl text-slate-200 pl-7">List of Books</h1>
+        </header>
