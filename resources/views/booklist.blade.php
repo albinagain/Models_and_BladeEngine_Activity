9f4bb1dc8a9daf018e9af7b4237a785d2ef7282a
albin <ajmmosawan@gmail.com>
2024-04-08 14:05:06 +0800

    added contents of views

--- resources/views/booklist.blade.php
+++ resources/views/booklist.blade.php
@@ -1,3 +1,28 @@
-<div>
-    <!-- Because you are alive, everything is possible. - Thich Nhat Hanh -->
-</div>
+@include('partials/header')
+
+<table>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>ISBN</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Description</th>
+            <th>Date Published</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($data as $book)
+            <tr>
+                <td>{{ $book->id }}</td>
+                <td>{{ $book->isbn }}</td>
+                <td>{{ $book->title }}</td>
+                <td>{{ $book->author }}</td>
+                <td>{{ $book->description }}</td>
+                <td>{{ $book->date_published }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+
+@include('partials/footer')
