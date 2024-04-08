d03cb21f137bbc787c2e99a8948d7b9f21685c29
albin <ajmmosawan@gmail.com>
2024-04-08 14:52:19 +0800

    changed font weight of the text in head and footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="bg-blue-600 h-16 flex flex-col justify-center items-end">
-    <h1 class="text-3xl text-slate-200 pr-7">List of Books</h1>
+    <h1 class="text-3xl text-slate-200 pr-7 font-bold">List of Books</h1>
 </footer>
 </div>
 </body>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -10,5 +10,5 @@
 <body class="bg-slate-200">
     <div class="container mx-auto">
         <header class="bg-blue-600 h-16 flex flex-col justify-center items-start">
-            <h1 class="text-3xl text-slate-200 pl-7">List of Books</h1>
+            <h1 class="text-3xl text-slate-200 pl-7 font-bold">List of Books</h1>
         </header>
