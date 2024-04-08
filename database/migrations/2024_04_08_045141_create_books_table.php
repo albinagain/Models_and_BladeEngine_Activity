5d5bb812175b35c3acdffa253f71d798b8f982e8
albin <ajmmosawan@gmail.com>
2024-04-08 13:17:57 +0800

    changed isbn column into string

--- database/migrations/2024_04_08_045141_create_books_table.php
+++ database/migrations/2024_04_08_045141_create_books_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
-            $table->integer('isbn')->unique();
+            $table->string('isbn', 13)->unique();
             $table->string('title');
             $table->string('author');
             $table->string('description');
